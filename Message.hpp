823d8ae5c72f3ba84198d8f80afd4088ed26b6ed
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-03-06 15:08:49 +0900

    [Fix] Message Class 생성자 변경 close #11

--- Message.hpp
+++ Message.hpp
@@ -2,16 +2,18 @@
 # define MESSAGE_HPP
 
 # include <string>
+# include <vector>
 
 class Message {
 	private:
+		std::vector<int> _targets;
 		int _code;
 		std::string _prefix;
 		std::string _command;
 		std::string _params;
 
 	public:
-		Message(std::string str);
+		Message(std::vector<int> targets, int code, std::string prefix, std::string command, std::string params = "");
 		~Message();
 		const int getCode() const;
 		const std::string getPrefix() const;
