a60c0aba957463fa84ca562c49753b9edb9699a7
sumnii <sumsong@student.42seoul.kr>
2023-03-06 15:33:36 +0900

    [Refact] err_exit 함수로 에러 핸들링

--- srcs/socket.cpp
+++ srcs/socket.cpp
@@ -3,6 +3,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
+
+void err_exit(std::string error_msg)
+{
+	std::cerr << "Error " << error_msg << std::endl;
+	exit(1);
+}
 
 int main()
 {
@@ -11,10 +18,7 @@ int main()
 
 	server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (server_socket == -1)
-	{
-		std::cerr << "Error creating socket: " << strerror(errno) << std::endl;
-		return 1;
-	}
+		err_exit("creating socket : " + std::string(strerror(errno)));
 
 	bzero(&server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
@@ -22,17 +26,13 @@ int main()
 	server_addr.sin_port = htons(8080);
 
 	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-	{
-		std::cerr << "Error binding socket: " << strerror(errno) << std::endl;
-		return 1;
-	}
+		err_exit("binding socket : " + std::string(strerror(errno)));
 
 	if (listen(server_socket, 10) == -1)
-	{
-		std::cerr << "Error socket listening: " << strerror(errno) << std::endl;
-		return 1;
-	}
+		err_exit("socket listening : " + std::string(strerror(errno)));
+
+	if (fcntl(server_socket, F_SETFL, O_NONBLOCK) == -1)
+		err_exit("setting socket flag : " + std::string(strerror(errno)));
 
 	std::cout << "Connected to server" << std::endl;
-	return 0;
 }
