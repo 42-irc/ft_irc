0ed491abafbb817f59223c327e21e161f256204e
NamJun Heo <90181905+nheo9143@users.noreply.github.com>
2023-03-08 16:33:32 +0900

    [Refactor] 개행 추가

--- Server.cpp
+++ Server.cpp
@@ -4,11 +4,14 @@ std::map<std::string, Channel> Server::_channels;
 std::map<std::string, User> Server::_users;
 
 Server::Server(int port, std::string password, std::string adminName, std::string adminPassword) : _port(port), _password(password), _adminName(adminName), _adminPassword(adminPassword) {};
+
 Server::~Server() {};
+
 const int Server::getPort() const { return (_port); }
+
 const std::map<std::string, Channel> Server::getChannels() { return (_channels); };
-const Channel Server::findChannel(User client, std::string name)
-{
+
+const Channel Server::findChannel(User client, std::string name) {
 	std::map<std::string, Channel>::iterator it = _channels.find(name);
 	if (it != _channels.end())
 		return (it->second);
@@ -18,8 +21,7 @@ const Channel Server::findChannel(User client, std::string name)
 	return (Channel());
 };
 const std::map<std::string, User> Server::getUsers() { return (_users); };
-const User Server::findUser(User client, std::string name)
-{
+const User Server::findUser(User client, std::string name) {
 	std::map<std::string, User>::iterator it = _users.find(name);
 	if (it != _users.end())
 		return (it->second);
@@ -28,23 +30,26 @@ const User Server::findUser(User client, std::string name)
 	throw Message(fd, 401, "ft_irc", "ERROR", name + " :No such nick");
 	return (User());
 };
+
 const std::string Server::getPassword() const { return (_password); };
+
 const std::string Server::getAdminName() const { return (_adminName); };
+
 const std::string Server::getAdminPassword() const { return (_adminPassword); };
-void Server::addChannel(Channel channel)
-{
+
+void Server::addChannel(Channel channel) {
 	_channels.insert(std::pair<std::string, Channel>(channel.getName(), channel));
 }
-void Server::removeChannel(Channel channel)
-{
+
+void Server::removeChannel(Channel channel) {
 	_channels.erase(channel.getName());
 };
-void Server::addUser(User user)
-{
+
+void Server::addUser(User user) {
 	_users.insert(std::pair<std::string, User>(user.getNickName(), user));
 }
-void Server::removeUser(User user)
-{
+
+void Server::removeUser(User user) {
 	std::map<std::string, Channel>::iterator first = _channels.begin();
 	std::map<std::string, Channel>::iterator last = _channels.end();
 	while (first != last)
