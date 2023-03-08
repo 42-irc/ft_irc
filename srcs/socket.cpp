bb2c5c78c04c164a396e5c869396f58e2381c161
sumnii <sumsong@student.42seoul.kr>
2023-03-08 15:08:27 +0900

    [Feat] 서버 소켓의 read 이벤트에 대한 kevent 세팅 #37

--- srcs/socket.cpp
+++ srcs/socket.cpp
@@ -4,6 +4,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include <sys/event.h>
 
 void err_exit(std::string error_msg)
 {
@@ -28,11 +29,47 @@ int main()
 	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
 		err_exit("binding socket : " + std::string(strerror(errno)));
 
-	if (listen(server_socket, 10) == -1)
+	if (listen(server_socket, 5) == -1)
 		err_exit("socket listening : " + std::string(strerror(errno)));
 
 	if (fcntl(server_socket, F_SETFL, O_NONBLOCK) == -1)
 		err_exit("setting socket flag : " + std::string(strerror(errno)));
 
-	std::cout << "Connected to server" << std::endl;
+	int kq = kqueue();
+	if (kq == -1)
+		err_exit("creating kqueue : " + std::string(strerror(errno)));
+
+	struct kevent events[2];
+	EV_SET(&events[0], server_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);
+
+	std::cout << "Waiting for incoming connections..." << std::endl;
+	{
+		int nev = kevent(kq, events, 1, events, 2, NULL);
+		if (nev == -1)
+			err_exit("calling kevent : " + std::string(strerror(errno)));
+
+		for (int i = 0; i < nev; ++i)
+		{
+			// 서버 소켓에 READ 이벤트 발생 시
+			if (events[i].ident == server_socket)
+			{
+				std::cout << "READ event occurs in server_socket!" << std::endl;
+
+				int client_socket = accept(server_socket, NULL, NULL);
+				if (client_socket == -1)
+					err_exit("accepting client : " + std::string(strerror(errno)));
+
+				std::cout << client_socket << ": New client connected" << std::endl;
+
+				if (fcntl(client_socket, F_SETFL, O_NONBLOCK) == -1)
+					err_exit("setting client socket flag : " + std::string(strerror(errno)));
+
+				EV_SET(&events[1], client_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);
+			}
+			else
+			{
+				// 클라이언트 소켓에 read 이벤트 발생 시
+			}
+		}
+	}
 }
