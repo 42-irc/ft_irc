c010bdf0ad5b735db7d286150938f7345e4d098d
sumnii <sumsong@student.42seoul.kr>
2023-03-09 21:16:18 +0900

    [Feat] 클라이언트 소켓 생성 시 호스트네임까지 넣어서 서버에 추가 #80

--- srcs/client_socket.cpp
+++ srcs/client_socket.cpp
@@ -1,5 +1,4 @@
 #include "client_socket.hpp"
-#include "netdb.h"
 
 void create_client_socket(int server_socket, int kq) {
 	struct sockaddr_in client_addr;
@@ -11,16 +10,21 @@ void create_client_socket(int server_socket, int kq) {
 	char client_host[NI_MAXHOST];
 	getnameinfo((struct sockaddr*)&client_addr, client_addr_size, client_host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
 	
-	std::cout << client_host << std::endl;
-	std::cout << new_client_socket << ": New client connected\n" << std::endl;
+	std::cout << client_host << ": New client connected\n" << std::endl;
 
 	if (fcntl(new_client_socket, F_SETFL, O_NONBLOCK) == -1)
 		err_exit("setting client socket flag : " + std::string(strerror(errno)));
 
 	Client client;
+	client.setHostName(client_host);
+	Server::addClient(client);
 
 	struct kevent client_socket_event[2];
 	EV_SET(&client_socket_event[0], new_client_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);
-	EV_SET(&client_socket_event[1], new_client_socket, EVFILT_WRITE, EV_ADD, 0, 0, NULL);
+	EV_SET(&client_socket_event[1], new_client_socket, EVFILT_WRITE, EV_ONESHOT, 0, 0, NULL);
 	kevent(kq, &client_socket_event[0], 2, NULL, 0, NULL);
+
+	// 임시 접속 메시지
+	char message[] = "🍀 WELCOME TO IRC SERVER 🍀\n";
+	send(new_client_socket, message, sizeof(message), 0);
 }
