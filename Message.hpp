f6f7d8a9dabd6c675d4788e7fcd62bfa0164dd3a
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-03-09 22:53:49 +0900

    [Fix] Message의 _code unsinged int로 변경C

--- Message.hpp
+++ Message.hpp
@@ -1,19 +1,20 @@
 #ifndef MESSAGE_HPP
 # define MESSAGE_HPP
 
-# include <string>
-# include <vector>
+#include <iostream>
+#include <vector>
+#include "srcs/utils.hpp"
 
 class Message {
 	private:
 		std::vector<int> _targets;
-		int _code;
+		unsigned int _code;
 		std::string _prefix;
 		std::string _content;
 
 	public:
 		Message();
-		Message(std::vector<int> targets, int code, std::string content);
+		Message(std::vector<int> targets, unsigned int code, std::string content);
 		Message(std::vector<int> targets, std::string prefix, std::string content);
 		~Message();
 		const std::string getMessage();
