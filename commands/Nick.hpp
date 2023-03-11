bbac2cfcd47dfe3095397152d5a00306747d6262
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-03-11 18:29:02 +0900

    [Feat] Nick 구현 close #86

--- commands/Nick.hpp
+++ commands/Nick.hpp
@@ -1,16 +1,18 @@
 #ifndef NICK_HPP
 # define NICK_HPP
 
-# include <string>
 # include "Command.hpp"
+# include "../Server.hpp"
 
 class Nick : public Command
 {
 	private:
 		std::string _nick;
+		std::string getPrefix(std::string oldNick) const;
+		void checkFirstNick();
 
 	public:
-		Nick(std::string nick, Client client);
+		Nick(Client* client, std::string nick);
 		~Nick();
 		std::vector<Message> execute();
 };
