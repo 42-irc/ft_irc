18bf1db034a1fef1bef1d7ca9c568b9078ac47f3
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-03-13 02:06:57 +0900

    [Feat] 유저가 클라이언트 종료 시 채널에 남은 유저들에게 메시지 전송 close #69

--- commands/Quit.cpp
+++ commands/Quit.cpp
@@ -4,9 +4,20 @@ Quit::Quit(Client* client) : Command(client, "QUIT") {}
 
 Quit::~Quit() {}
 
+/*
+:<nick>!<user>@<host> QUIT [<Quit message>]
+*/
 std::vector<Message> Quit::execute() {
+	std::vector<Message> messages;
+	std::set<std::string> channels = _client->getJoinedChannels();
+	std::set<std::string>::iterator it = channels.begin();
+	std::set<std::string>::iterator ite = channels.end();
+
+	for (; it != ite; it++) {
+		Channel* channel = Server::findChannel(_client, *it);
+		messages.push_back(Message(channel->getFdsExceptClient(_client), getPrefix(), _type));
+	}
 	Server::removeClient(_client);
-	
-	return std::vector<Message>();
+	return messages;
 }
 
