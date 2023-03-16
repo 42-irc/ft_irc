e3fcd6a6118c9d66c692966f4d8290bd2b579cb9
sumnii <sumsong@student.42seoul.kr>
2023-03-16 13:07:53 +0900

    [Refact] pingTime 관련 프로퍼티/메소드 삭제

--- srcs/client/Client.hpp
+++ srcs/client/Client.hpp
@@ -18,7 +18,6 @@ class Client {
 		std::set<std::string> _joinedChannels;
 		bool _isVerified;
 		bool _isAdmin;
-		time_t _lastPingTime;
 
 	public:
 		Client();
@@ -34,14 +33,12 @@ class Client {
 		int getFd() const;
 		bool getIsAdmin() const;
 		bool getIsVerified() const;
-		time_t getLastPingTime() const;
 
 		void setName(const std::string& name);
 		void setNickName(const std::string& nickName);
 		void setHostName(const std::string& hostName);
 		void setIsAdmin(const bool isAdmin);
 		void setIsVerified(const bool isVerified);
-		void setLastPingTime(const size_t pingTime);
 
 		void joinChannel(const std::string& target);
 		void leaveChannel(const std::string& target);
