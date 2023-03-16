ef3e7030f358c78c4798b40ce49f97d612aa40cb
tamagoyakii <rlawlgus4063@naver.com>
2023-03-16 18:13:56 +0900

    [Delete] Message catch 하는 부분 삭제

--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -136,12 +136,10 @@ void Server::execute() {
 					for (; it != ite; it++) {
 						try {
 							Command* command = parse(findClient(clientSocket), *it);
-
 							command->execute();
 							delete command;
-						} catch (Message& e) {
-							e.sendMessage();
 						} catch (std::exception& e) {
+							// delete command 처리해야 함
 							continue ;
 						}
 					}
